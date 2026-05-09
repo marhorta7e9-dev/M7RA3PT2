6d21917c76c5815de4e718ae48c3a9691d037a08
marhorta <mar.horta.7e9@itb.cat>
2026-05-09 12:32:44 +0200

    Update CreateAssetMenu attribute for PlayerScriptableObject

--- Assets/Scripts/PlayerScriptableObject.cs
+++ Assets/Scripts/PlayerScriptableObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
+[CreateAssetMenu(fileName = "New Player", menuName = "Scriptable Objects/NewPlayer")]
 public class PlayerScriptableObject : ScriptableObject
 {
     public string nom;
